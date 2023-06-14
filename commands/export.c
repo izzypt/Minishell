22f678857610ebb481963e6dd41f638b3964cedd
Simao <bonvalots@gmail.com>
2023-06-14 20:41:14 +0100

    export env variables

--- commands/export.c
+++ commands/export.c
@@ -6,24 +6,34 @@
 /*   By: simao <simao@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 12:54:51 by simao             #+#    #+#             */
-/*   Updated: 2023/06/14 15:45:13 by simao            ###   ########.fr       */
+/*   Updated: 2023/06/14 20:36:34 by simao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../srcs/minishell.h"
 
+/*
+- Adds a new node to the list of env variables.
+*/
 void	cmd_export(char *variable)
 {
 	char	**input;
-	int		i;
+	char	*pair;
+	char	**key_value;
+	t_env	*lst;
 
-	i = 0;
+	lst = get_env();
 	if (!ft_strchr(variable, '='))
 		return ;
 	input = ft_split(variable, ' ');
-	while (data()->env_var[i] != NULL)
-		i++;
-	data()->env_var[i] = input[1];
-	i++;
-	data()->env_var[i] = NULL;
+	pair = input[1];
+	key_value = ft_split(pair, '=');
+	while (lst->nxt != NULL)
+		lst = lst->nxt;
+	lst->nxt = malloc(sizeof(t_env));
+	lst = lst->nxt;
+	lst->key = key_value[0];
+	lst->value = key_value[1];
+	lst->pair = pair;
+	lst->nxt = NULL;
 }
