003f42b518a87adb61585e6ee9dfc5f5237753f1
Simao <bonvalots@gmail.com>
2023-06-16 16:53:04 +0100

    builtin cd command

--- commands/cd.c
+++ commands/cd.c
@@ -6,7 +6,7 @@
 /*   By: simao <simao@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 16:57:39 by simao             #+#    #+#             */
-/*   Updated: 2023/06/15 12:06:11 by simao            ###   ########.fr       */
+/*   Updated: 2023/06/16 16:51:47 by simao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,25 @@
 */
 void	cmd_cd(char *path)
 {
-	if (chdir(path) != 0)
+	char	cwd[1024];
+	char	**path1;
+	char	*oldpwd;
+
+	path1 = ft_split(path, ' ');
+	oldpwd = ft_strdup(getcwd(cwd, sizeof(cwd)));
+	if (chdir(path1[1]) != 0)
 	{
-		if (access(path, F_OK) == -1)
+		printf("chdir(path) : %d\n", chdir(path));
+		if (access(path1[1], F_OK) == -1)
 			write(2, "minishell: cd: no such file or directory\n", 42);
-		else if (access(path, R_OK | W_OK | X_OK) == -1)
+		else if (access(path1[1], R_OK | W_OK | X_OK) == -1)
 			write(2, "minishell: cd: permission denied\n", 34);
 		else
 			write(2, "minishell: cd: not a directory\n", 32);
-		write(2, path, ft_strlen(path));
-		write(2, "\n", 1);
+	}
+	else
+	{
+		replace_env_var("OLDPWD", oldpwd);
+		replace_env_var("PWD", getcwd(cwd, sizeof(cwd)));
 	}
 }
