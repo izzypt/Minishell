3878b867d516e2d7c56eae5bdb8c0044102716db
Simao <bonvalots@gmail.com>
2023-08-09 12:26:25 +0100

    adding protection to symbols functions if no node or no token

--- utils/symbols.c
+++ utils/symbols.c
@@ -6,7 +6,7 @@
 /*   By: simao <simao@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 11:27:25 by smagalha          #+#    #+#             */
-/*   Updated: 2023/08/09 11:58:24 by simao            ###   ########.fr       */
+/*   Updated: 2023/08/09 12:25:29 by simao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	its_output(t_list *node)
 {
+	if (!node || !node->token)
+		return (0);
 	if (!ft_strncmp(node->token[0], ">", 2))
 		return (1);
 	return (0);
@@ -21,11 +23,16 @@ int	its_output(t_list *node)
 
 int	its_input(t_list *node)
 {
+	if (!node || !node->token)
+		return (0);
 	if (!ft_strncmp(node->token[0], "<", 2))
 		return (1);
 	return (0);
 }
 
+/*
+- This is the function used in parsing. DIfferent from "its_a_pipe"
+*/
 int	is_pipe(char *token)
 {
 	int	len;
@@ -41,6 +48,8 @@ int	is_pipe(char *token)
 
 int	its_a_pipe(t_list *node)
 {
+	if (!node || !node->token)
+		return (0);
 	if (!ft_strncmp(node->token[0], "|", 2))
 		return (1);
 	return (0);
@@ -48,6 +57,8 @@ int	its_a_pipe(t_list *node)
 
 int	its_append(t_list *node)
 {
+	if (!node || !node->token)
+		return (0);
 	if (!ft_strncmp(node->token[0], ">>", 3))
 		return (1);
 	return (0);
@@ -55,6 +66,8 @@ int	its_append(t_list *node)
 
 int	its_heredoc(t_list *node)
 {
+	if (!node || !node->token)
+		return (0);
 	if (!ft_strncmp(node->token[0], "<<", 3))
 		return (1);
 	return (0);
